d64d59cd0e8d47bd9affb7ebab05af3735f2bf1a
Adam Izdebski <42173829+AdamIzdebski@users.noreply.github.com>
2019-08-05 15:19:03 +0200

    Short description forced for less than 3 variables
    * Short description forced for less than 3 variables
    
    * add small_model test

--- R/describe_breakdown.R
+++ R/describe_breakdown.R
@@ -105,7 +105,8 @@ describe.break_down <- function(explainer,
 
   if (is.null(label)) label = "the prediction for the selected instance is"
 
-  if (short_description) {
+  if (short_description | nrow(explainer) < 5) {
+    # If explainer contains less than three variables, short description is forced
     descriptions <- make_short_description(explainer, display_values, label, model_name, description_profile)
   } else {
     introduction <- make_introduction(explainer,
@@ -212,7 +213,7 @@ describe_distribution <- function(explainer,
     quartile <- which(predictions_quartile < prediction)[length(which(predictions_quartile < prediction))][[1]]
     quartile_which <- if (quartile == 1) "first" else if (quartile == 2) "second" else if (quartile == 3) "third" else "fourth"
 
-    description <- paste0("Model predictions ranges from ", round(min(model_predictions),3), " to ", round(max(model_predictions), 3), ". The distribution of ", model_name, "'s predictions is ", skeweness, " with average equal to ", mean , " and median equal to ", median, ". The standard deviation is ", sd, ". Model's prediction for the selected instance is in the ", quartile_which, " quartile.")
+    description <- paste0("Model predictions range from ", round(min(model_predictions),3), " to ", round(max(model_predictions), 3), ". The distribution of ", model_name, "'s predictions is ", skeweness, " with average equal to ", mean , " and median equal to ", median, ". The standard deviation is ", sd, ". Model's prediction for the selected instance is in the ", quartile_which, " quartile.")
   }
   description
 }
@@ -292,7 +293,7 @@ make_argument <- function(explainer,
     df <- explainer[-c(1,nrow(explainer)), c("variable_name","contribution", "variable_value")]
     df['importance'] <- abs(df$contribution)
     df <- df[order(df$importance, decreasing = TRUE), ] # We do not cut arguments with importance below the treshold
-    df[1:3,'order'] <- c("", "second ", "third ") # We choose three most significant variables
+    nrow_df <- min(3, nrow(df))
     df['contribution'] <- round(df['contribution'], 3)
     df['shap'] <- "" # needed for shap explanation
 
@@ -318,27 +319,41 @@ make_argument <- function(explainer,
           df[df$variable_name == x, 'shap'] <- is_important[x]})
     }}
     if (show_shap) {
-      shap <-paste(df[1:3, 'shap'],collapse = "")
+      shap <-paste(df[1:nrow_df, 'shap'],collapse = "")
       shap <- ifelse(shap =="",
-                     "The average contribution of all the above variable's is significant.",
+                     "The average contribution of all the above variables is significant.",
                       paste0("From the above variables ",
                       paste(df[!(df$shap == ""), 'shap'], collapse = ", "),
-                      " may not be significant may not be significant",
-                      "duu to high average contribution dispersion."))
+                      " may not be significant",
+                      " due to high average contribution dispersion."))
     }
 
     if (display_numbers) {
       sign1 <- if (df$contribution[1] > 0) "increases" else "decreases"
-      argument1 <- paste0("The ", {df$order[1]}, "most important variable is ", {df$variable_name[1]}, ". It ",{sign1}, " the prediction by ", {abs(df$contribution[1])}, ".", df$shap[1])
-      sign2 <- if (df$contribution[2] > 0) "increases" else "decreases"
-      argument2 <- paste0("The ", {df$order[2]}, "most important variable is ", {df$variable_name[2]},". It ", {sign2}, " the prediction by ", {abs(df$contribution[2])}, ".", df$shap[2])
-      sign3 <- if (df$contribution[3] > 0) "increases" else "decreases"
-      argument3 <- paste0("The ", {df$order[3]}, "most important variable is ", {df$variable_name[3]},". It ", {sign3}, " the prediction by ", {abs(df$contribution[3])}, ".", df$shap[3])
-      argumentation <- ifelse(show_shap,
-                              paste(argument1, argument2, argument3, shap, sep = " \n"),
-                              paste(argument1, argument2, argument3, sep = " \n"))
+      argument1 <- paste0("The most important variable is ", {df$variable_name[1]}, ". It ",{sign1}, " the prediction by ", {abs(df$contribution[1])}, ".", df$shap[1])
+      if (nrow(df) == 1) {
+        argumentation <- ifelse(show_shap,
+                                paste(argument1, shap, sep = " \n"),
+                                paste(argument1, sep = " \n"))
+      }
+      if (nrow(df) > 1) {
+        sign2 <- if (df$contribution[2] > 0) "increases" else "decreases"
+        argument2 <- paste0("The second most important variable is ", {df$variable_name[2]},". It ", {sign2}, " the prediction by ", {abs(df$contribution[2])}, ".", df$shap[2])
+      }
+      if (nrow(df) == 2) {
+        argumentation <- ifelse(show_shap,
+                                paste(argument1, argument2, shap, sep = " \n"),
+                                paste(argument1, argument2, sep = " \n"))
+      }
+      if (nrow(df) > 2) {
+        sign3 <- if (df$contribution[3] > 0) "increases" else "decreases"
+        argument3 <- paste0("The third most important variable is ", {df$variable_name[3]},". It ", {sign3}, " the prediction by ", {abs(df$contribution[3])}, ".", df$shap[3])
+        argumentation <- ifelse(show_shap,
+                                paste(argument1, argument2, argument3, shap, sep = " \n"),
+                                paste(argument1, argument2, argument3, sep = " \n"))
+      }
     } else {
-      df <- df[1:3, ]
+      df <- df[1:nrow_df, ]
       df_positive <- df[which(df$contribution >= 0), ]
       df_negative <- df[which(df$contribution < 0), ]
       prefix_pos <- if (nrow(df_positive) > 1) "are" else "is"
--- tests/testthat/test_describe.R
+++ tests/testthat/test_describe.R
@@ -13,12 +13,20 @@ explain_titanic_rf <- explain(model_titanic_rf,
                               y = titanic$survived == "yes",
                               label = "Random Forest v7")
 random_passanger <- titanic[sample(nrow(titanic),1),c(1,2,3,4,6,7,8)]
-rf_la <- break_down(explain_titanic_rf, random_passanger, keep_distributions = TRUE)
 
-description <- describe(rf_la)
+
+rf_la <- break_down(explain_titanic_rf, random_passanger, keep_distributions = TRUE)
 test_that("Output format", {
-  expect_is(description, "break_down_description")
+  expect_is(iBreakDown::describe(rf_la), "break_down_description")
 })
+
+rf_la <- break_down(explain_titanic_rf, random_passanger, keep_distributions = FALSE)
+test_that("Output format", {
+  expect_is(iBreakDown::describe(rf_la), "break_down_description")})
+
+
+test_that("Output format", {
+  expect_error(describe(rf_la, display_values = 4))})
 
 n <- 4
 test <- expand.grid(replicate(n, c(TRUE,FALSE), simplify = FALSE))
@@ -53,5 +61,31 @@ test_result_shap <- apply(test, MARGIN = 1, function(x){
   })
 })
 
+## test for less than 3 variables
+titanic_small <- titanic[sample(1:nrow(titanic), 500),]
 
+
+model_small <- glm(survived == "yes" ~ age + gender,
+                   data = titanic_small[, c(1,2,9)],
+                   family = "binomial")
+
+explain_model_small <- explain(model_small,
+                               data = titanic_small[, c(1,2)],
+                               y = titanic_small$survived == "yes")
+
+
+shap_small <- shap(explain_model_small, new_observation = titanic_small[1,c(1,2)])
+bd_small <- break_down(explain_model_small, new_observation = titanic_small[1,c(1,2)])
+
+test_that("Output format", {
+  expect_is(iBreakDown::describe(shap_small, display_numbers = TRUE,
+                                 display_values = TRUE, display_shap = TRUE)
+            ,"break_down_description")
+})
+
+test_that("Output format", {
+  expect_is(iBreakDown::describe(bd_small, display_numers = TRUE,
+                                 display_values = TRUE)
+            ,"break_down_description")
+})
 
